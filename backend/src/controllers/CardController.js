93dd7c3a2838d134708175848889caa70350b786
Jonathan santos <jonathandossantos.c@gmail.com>
2019-08-18 22:25:11 -0300

    feat: listing cards

--- backend/src/controllers/CardController.js
+++ backend/src/controllers/CardController.js
@@ -1,15 +1,13 @@
 const Card = require('../models/Card')
 
-
 module.exports = {
     async index(req, res) {
-        console.log('oi index');
-
-        return null
+        const cards = await Card.find()
+        // console.log()
+        return res.json(cards)
     },
     async store(req, res) {
         const { front, back } = req.body;
-        console.log(front, back)
 
         const card = await Card.create({
             front: front,
@@ -17,5 +15,15 @@ module.exports = {
         })
 
         return res.json(card)
+    },
+    async delete(req, res) {
+        console.log('id', req.headers.id)
+        const db = req.db
+        
+        console.log(db.collection('cards'))
+        // db.collection('cards').removeOne({"_id": objectId(req.headers.id)}, (err, result) => {
+        // if (err) return console.log('oi',err)
+        // })
+        return res.json({"message": "Success"})
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,7 @@ const CardController = require('./controllers/CardController')
 const routes = express.Router();
 
 routes.get('/cards', CardController.index)
-
 routes.post('/cards', CardController.store)
+routes.delete('/deleteCards', CardController.delete)
 
 module.exports = routes;
