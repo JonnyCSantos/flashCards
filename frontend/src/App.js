f765824c6266cca7a891bf36fe26705c5624c795
Jonathan Santos <jonathan.santos@auaha.com.br>
2019-08-20 14:47:17 -0300

    feat: configuring material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import './App.css';
 
 function App() {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div className="App">
-
+      <div className={classes.root}>
+        <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>xs=12</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>xs=6</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>xs=6</Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
     </div>
   );
 }
